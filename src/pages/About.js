87427a9aa61733c2a59c2ad441c370d26d4a115c
Tess <tess.mueske@gmail.com>
2024-05-16 14:02:40 +0200

    complete lab

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+function About() {
+    return (
+      <>
+        <header>
+        </header>
+        <main>
+          <h1>This is my about component!</h1>
+        </main>
+      </>
+    );
+  };
+  
+  export default About;
---
+++ src/pages/Login.js
@@ -0,0 +1,26 @@
+function Login() {
+    return (
+      <>
+        <header>
+        </header>
+        <main>
+          <h1>Login</h1>
+          <form>
+            <div>
+              <label for="username">Username: </label>
+              <input id="username" type="text" name="username" placeholder="Username" />
+            </div>
+            <br/>
+            <div>
+              <label for="password">Password: </label>
+              <input id="password" type="password" name="password" placeholder="Password" />
+            </div>
+            <br/>
+            <button type="submit">Submit</button>
+          </form>
+        </main>
+      </>
+    );
+  };
+  
+  export default Login;
